502ccf3cc70121232a011dade1ccf99ebdf06887
istrejo <istrejo2106@gmail.com>
2024-12-02 23:37:04 -0400

    profile page

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  selectAuthState,
+  selectUser,
+} from './../../state/selectors/auth.selectors';
+import { Component, inject, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +11,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
+  userInfo = null;
+  private store = inject(Store);
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.store.select(selectAuthState).subscribe((auth: any) => {
+      console.log(auth);
+      this.userInfo = auth;
+    });
   }
-
 }
