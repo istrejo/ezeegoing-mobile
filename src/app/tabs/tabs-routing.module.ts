c232ea6f0140b9cd5cedbdec498d4fd3aa176fd2
istrejo <atrejo@beartrack.cl>
2025-01-08 23:01:02 -0400

    Redirect to reserver page

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -44,7 +44,7 @@ const routes: Routes = [
       },
       {
         path: '',
-        redirectTo: '/tabs/reservations',
+        redirectTo: '/tabs/reserve',
         pathMatch: 'full',
       },
       {
